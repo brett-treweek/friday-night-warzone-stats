882920022ec8b0c7a7cf4cd0f3770ed31352df40
Brett Treweek <bretttrew@gmail.com>
2022-01-29 14:02:09 +0800

    trying to get html to image to work properly

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageAttachment } = require('discord.js');
+const nodeHtmlToImage = require('node-html-to-image');
 const axios = require('axios');
 const userDeets = require('../userDeets');
 const rateLimit = require('axios-rate-limit');
@@ -21,7 +22,8 @@ const http = rateLimit(axiosInstance, {
 const average = function(x, y) {
 	if (isNaN(x / y)) {
 		return 0;
-	} return (x / y).toFixed(3);
+	}
+	return (x / y).toFixed(3);
 };
 
 const getStats = function(sessionArray) {
@@ -46,6 +48,7 @@ module.exports = {
 		.setName('stats')
 		.setDescription('Replies with session stats'),
 	async execute(interaction) {
+
 		const statsEmbed = new MessageEmbed()
 			.setColor('#00F0FF')
 			.setTitle('Session Stats')
@@ -59,15 +62,11 @@ module.exports = {
 
 		const sessionArray = userDeets.map((x) => x);
 		const initialArray = require('../initialArray');
-
-		console.log('initialArray in stats command!', initialArray);
+		let cards = '';
 
 		await interaction.deferReply();
 		getStats(sessionArray);
-
 		await wait(8000);
-
-		console.log('Session Stats:', sessionArray);
 
 		sessionArray.map((s) => {
 			if (s.stats === undefined) {
@@ -77,48 +76,131 @@ module.exports = {
 			} else {
 				initialArray.map((i) => {
 					if (s.userName === i.userName) {
-						{
-							statsEmbed.addFields(
-								{
-									name: `============ ${s.userName} ============`,
-									value: '\u200B',
-									inline: false,
-								},
-								{
-									name: 'Kills',
-									value: `${parseInt(
-										s.stats.kills - i.stats.kills
-									)}`,
-									inline: false,
-								},
-								{
-									name: 'Deaths',
-									value: `${parseInt(
-										s.stats.deaths - i.stats.deaths
-									)}`,
-									inline: false,
-								},
-								{
-									name: 'K/D',
-									value: `${average(
-										s.stats.kills - i.stats.kills,
-										s.stats.deaths - i.stats.deaths
-									)}`,
-									inline: false,
-								},
-								{
-									name: 'Downs',
-									value: `${parseInt(
-										s.stats.downs - i.stats.downs
-									)}`,
-									inline: false,
-								}
-							);
-						}
+						cards = cards.concat(
+							`<div class="card">
+								<h3>${s.userName}</h3>
+								<ul>
+									<li> Kills: ${parseInt(s.stats.kills - i.stats.kills)}</li>
+									<li> Deaths: ${parseInt(s.stats.deaths - i.stats.deaths)}</li>
+									<li> K/D: ${average(s.stats.kills - i.stats.kills, s.stats.deaths - i.stats.deaths)}</li>
+								</ul>
+							<div>`);
 					}
 				});
 			}
 		});
-		await interaction.editReply({ embeds: [statsEmbed] });
+
+		const template = `
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta http-equiv="X-UA-Compatible" content="IE=edge">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Document</title>
+				<style>
+					body {
+				height: 100vh;
+				font-family: Arial, Helvetica, sans-serif;
+                max-width: 300px;
+			}
+			h1 {
+				font-size: 25px;
+				font-weight: 400;
+			}
+			h3 {
+                letter-spacing: 0.1ch;
+				font-weight: 200;
+				font-size: 18px;
+                margin-bottom: 0;
+			}
+			.container {
+                max-width: 300px;
+                background-image: url('https://preview.redd.it/uiehsutl24h51.jpg?auto=webp&s=7cd5e5b778cbecc70ef0f54ef133cff7a1e20df3');
+				padding: 1rem;
+				/* height: 70vh; */
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				color: #fff;
+			}
+			.card {
+                width: 100%;
+				border-radius: 10px;
+				box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+				margin-bottom: 1rem;
+				display: flex;
+				flex-direction: column;
+				background-color: rgb(30, 80, 72);
+				align-items: center;
+                justify-content: center;
+			}
+			ul {
+				list-style: none;
+                padding: 0;
+                margin: 0;
+			}
+			li {
+                margin: 1rem;
+				font-size: 12px;
+			}
+				</style>
+			</head>
+			<body>
+				<div class="container">
+					<h1>Warzone Session Stats</h1>
+					${cards}
+				</div>
+			</body>
+			</html>
+		`;
+
+		const images = await nodeHtmlToImage({
+			html: template,
+			quality: 100,
+			type: 'jpeg',
+			puppeteerArgs: {
+				args: ['--no-sandbox'],
+			},
+			encoding: 'buffer',
+		});
+
+		const attatch = new MessageAttachment(images, 'brett.jpeg');
+
+		await interaction.editReply({ files:  [attatch] });
 	},
 };
+
+// {
+// 	statsEmbed.addFields(
+// 		{
+// 			name: `============ ${s.userName} ============`,
+// 			value: '\u200B',
+// 			inline: false,
+// 		},
+// 		{
+// 			name: 'Kills',
+// 			value: `${parseInt(s.stats.kills - i.stats.kills)}`,
+// 			inline: false,
+// 		},
+// 		{
+// 			name: 'Deaths',
+// 			value: `${parseInt(s.stats.deaths - i.stats.deaths)}`,
+// 			inline: false,
+// 		},
+// 		{
+// 	name: 'K/D',
+// 	value: `${average(
+// 		s.stats.kills - i.stats.kills,
+// 		s.stats.deaths - i.stats.deaths
+// 	)}`,
+// 	inline: false,
+// },
+// 		{
+// 			name: 'Downs',
+// 			value: `${parseInt(s.stats.downs - i.stats.downs)}`,
+// 			inline: false,
+// 		}
+// 	);
+// }
